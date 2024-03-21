f38f48f7fea231b3420c1ad65f558c368bed0f33
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2024-03-21 08:55:01 -0300

    att

--- index.dart
+++ index.dart
@@ -2,7 +2,7 @@ void main() {
   // Variável de tipo dinâmico
 var nome = "Dieimes";
   
-  //Variáveis com tipo específicos
+  // Variáveis com tipo específicos
   int idade = 33;
   double altura = 1.75;
   String cidade = 'Jardim Alegre';
